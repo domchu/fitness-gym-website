848eb59670216cb3edc3dad737872a04da12a88f
Mr.Dominic <ochu.dominic.onah@gmail.com>
2024-01-12 23:50:08 +0100

    chore:the media screens set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,40 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        "gray-20": "#F8F4EB", 
-        "gray-50": "#EFE6E6", 
-        "gray-100": "#DFCCCC", 
+        "gray-20": "#F8F4EB",
+        "gray-50": "#EFE6E6",
+        "gray-100": "#DFCCCC",
         "gray-500": "#5E0000",
-        "primary-100": "FFE1E0",
-        // "primary-300": "FFA6A3",
-        "primary-300": "#000000",
-        // "primary-500": "FF6B66",
-        "primary-500": "#f34",
-        "secondary-400": "FFCD58",
-        "secondary-500": "FFC132",        
+        "primary-100": "#FFE1E0",
+        "primary-300": "#FFA6A3",
+        "primary-500": "#FF6B66",
+        "secondary-400": "#FFCD5B",
+        "secondary-500": "#FFC132",
       },
       backgroundImage: (theme) => ({
-        "gradient-yellowred": "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
-        // "mobile-home": "url("./assets/imagename.png")"
+        "gradient-yellowred":
+          "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
+        "mobile-home": "url('./assets/HomePageGraphic.png')",
       }),
       fontFamily: {
-        dmsans: ["DM sans", "sans-serif"],
-        montserrat:["montserrat", "sans-serif"]
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
       },
       content: {
-        // evolvetext: "url("./assets/evolvetext.png")"
-        // abstractwaves: "url("./assets/abstractwaves.png")"
-        // sparkles: "url("./assets/sparkles.png")"
-        // circle: "url("./assets/circle.png")"
-      }
+        evolvetext: "url('./assets/EvolveText.png')",
+        abstractwaves: "url('./assets/AbstractWaves.png')",
+        sparkles: "url('./assets/Sparkles.png')",
+        circles: "url('./assets/Circles.png')",
+      },
     },
     screens: {
-      "xs": "480px",
-      "sm": "768px",
-      "md": "1060px",
-    }
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
   },
   plugins: [],
-}
+};
