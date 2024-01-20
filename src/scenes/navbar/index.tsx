7d4c7918e0dfe9ff01ff11063788dfd8ca676c95
Mr.Dominic <ochu.dominic.onah@gmail.com>
2024-01-20 14:48:56 +0100

    chore:finished the media screen of the application

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,5 +1,5 @@
 // import { useState } from "react";
-import { Bars3Icon } from "@heroicons/react/24/solid";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/logo.png"
 import Link from "./Link"
 import {SelectedPage} from "@/shared/types"
@@ -70,11 +70,46 @@ const Navbar = ({selectedPage, setSelectedPage}: Props) => {
                             </button>
                             )}
                           
-                  </div>
-                  
+                  </div>  
           </div>
           </div> 
 
+          
+          {/* MOBILE MENU MODAL */}
+          {!isAboveMediumQueryScreens && isMenuToggled && (
+              <div className="fixed right-0 bottom-0 h-full w-[250px] z-40 bg-purple-100 drop-shadow-xl">
+                  {/* CLOSE ICON */}
+                  <div className="flex justify-end px-12 py-8">
+                      <button onClick={() =>setIsMenuToggled(!isMenuToggled)}>
+                          <XMarkIcon className="h-6 w-10 text-gray-400" />
+                      </button>
+                  </div>
+                  {/* MENU ITEMS */}
+                          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+                              <Link
+                                  page="Home"
+                                  selectedPage={selectedPage}
+                                  setSelectedPage={setSelectedPage}
+                              />
+                              <Link
+                                  page="Benefits"
+                                  selectedPage={selectedPage}
+                                  setSelectedPage={setSelectedPage}
+                              />
+                              <Link
+                                  page="Contact Us"
+                                  selectedPage={selectedPage}
+                                  setSelectedPage={setSelectedPage}
+                              />
+                              <Link
+                                  page="Our Classes"
+                                  selectedPage={selectedPage}
+                                  setSelectedPage={setSelectedPage}
+                              />
+                          </div>
+                  
+              </div>
+          )}
     </nav>
   )
 }
