62655cc2d42a2f190a76f7a37804bd3bcb84920f
Mr.Dominic <ochu.dominic.onah@gmail.com>
2024-03-04 13:54:33 +0100

    fixed error on deployment

--- src/scenes/Footer/index.tsx
+++ src/scenes/Footer/index.tsx
@@ -1,4 +1,5 @@
 import Logo from "@/assets/Logo.png"
+import { motion } from 'framer-motion';
 
 
 
@@ -7,11 +8,20 @@ const Footer = () => {
         <footer className="bg-primary-100 py-20 mt-20">
             <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
 
-                <div className="basis-1/2 md:mt-0">
+                <motion.div className="basis-1/2 md:mt-0"
+                 initial="hidden"
+                 whileInView="visible"
+                 viewport={{ once: true, amount: 0.5 }}
+                 transition={{delay:0.2, duration: 1 }}
+                 variants={{
+                     hidden: { opacity: 0, x: -50 },
+                     visible:{opacity: 1, x:0}
+                 }}
+                >
                     <img src={Logo} alt="Brand Logo" />
                     <p className="py-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi debitis soluta similique, aperiam incidunt amet veniam alias rerum voluptatum totam.</p>
-                    <p>&copy; Evogym All Right Reserved</p>
-                </div>
+                    <p> &copy; Evogym All Right Reserved</p>
+                </motion.div>
 
                 <div className="basis-1/4 md:0">
                     <h4 className="font-bold">Links</h4>
